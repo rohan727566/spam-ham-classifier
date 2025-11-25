312e1bdcca2c4680fada66bffc7fda04942582db
Rohan Kumar <2k22.csai.2212823@gmail.com>
2025-11-25 20:38:51 +0530

    feat: add package structure and configuration module

---
+++ src/spam_classifier/__init__.py
@@ -0,0 +1,12 @@
+"""
+Spam/Ham Email Classification System
+Author: Rohan Kumar
+Training: AI Sep 2024 Batch (27th Oct)
+"""
+
+__version__ = "1.0.0"
+__author__ = "Rohan Kumar"
+
+from .config import Config
+
+__all__ = ["Config", "__version__", "__author__"]
